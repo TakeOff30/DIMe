c69bb8f4f9540f9a79bfa4b75cf4f58ed4c2bdd8
Oliver Stevanovic <oliver20669@gmail.com>
2023-02-20 23:25:01 +0100

    Login page setup

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,34 @@
 <script lang="ts">
+	import Router from 'svelte-spa-router';
+	import Home from './routes/Home.svelte';
+	import Profile from './routes/Profile.svelte';
+	import SearchPage from './routes/SearchPage.svelte';
+	import NotFound from './routes/NotFound.svelte';
+	import Login from './routes/Login.svelte';
+
+	let routes = {
+		'/': Home,
+		'/profile': Profile,
+		'/searchPage': SearchPage,
+		'*': NotFound,
+	};
+
+	let user;
 </script>
 
-<main />
+{#if user}
+	<nav>
+		<a href="#/">Home</a>
+		<a href="#/profile">Profile</a>
+		<a href="#/searchPage">Search</a>
+	</nav>
+
+	<main>
+		<Router {routes} />
+	</main>
+{:else}
+	<Login {user} />
+{/if}
 
 <style>
 </style>
---
+++ src/routes/Login.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let user;
+	let email = '';
+	let password = '';
+
+	// Import the functions you need from the SDKs you need
+	import { initializeApp } from 'firebase/app';
+	import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+	// TODO: Add SDKs for Firebase products that you want to use
+	// https://firebase.google.com/docs/web/setup#available-libraries
+
+	// Your web app's Firebase configuration
+	const firebaseConfig = {
+		apiKey: 'AIzaSyDvqx6aavkZ95vai95O19wu9tg3wCLFGtc',
+		authDomain: 'dime-20745.firebaseapp.com',
+		projectId: 'dime-20745',
+		storageBucket: 'dime-20745.appspot.com',
+		messagingSenderId: '1005405323846',
+		appId: '1:1005405323846:web:9eb6070bbe1c1d2e5c8047',
+	};
+
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+
+	const login = () => {
+		const auth = getAuth(app);
+		signInWithEmailAndPassword(auth, email, password)
+			.then((userCredentials) => {
+				const user = userCredentials.user;
+			})
+			.catch((error) => {});
+	};
+</script>
+
+<form>
+	<span>
+		<label>Email: </label>
+		<input bind:value={email} />
+	</span>
+	<span>
+		<label>password: </label>
+		<input bind:value={password} />
+	</span>
+	<button on:click={login}>Login</button>
+</form>
+
+<style>
+</style>
